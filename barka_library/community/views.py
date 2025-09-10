415896138aa4efbe2b1a3ce3698741ad11e66a95
Winifred Igboama <winifredigboama@gmail.com>
2025-09-10 09:06:20 +0100

    teach static files again

--- barka_library/community/views.py
+++ barka_library/community/views.py
@@ -1,21 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def community_events(request):
-    return HttpResponse("""<section class="community-events">
-  <h2>Upcoming Community Events</h2>
-  <ul>
-    <li>
-      <strong>Tech Meetup</strong> - September 10, 2025
-    </li>
-    <li>
-      <strong>Local Farmers' Market</strong> - September 15, 2025
-    </li>
-    <li>
-      <strong>Charity Fun Run</strong> - September 22, 2025
-    </li>
-    <li>
-      <strong>Book Club Gathering</strong> - September 28, 2025
-    </li>
-  </ul>
-</section>""")
+    return render(request, "community/community.html")
--- barka_library/main/views.py
+++ barka_library/main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to Barka Street Library</h1>")
+    return render(request, "main/home.html")
 
+def about(request):
+    return render(request, "main/about.html")
