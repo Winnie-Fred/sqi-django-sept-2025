a6a87a2a42d82415a25faaef183b199b8126d45e
Winifred Igboama <winifredigboama@gmail.com>
2025-09-22 09:52:05 +0100

    teach update and delete

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('add/book/', views.create_book, name='create_book'),
     path('add/book/manually-rendered/', views.create_book_manually_rendered, name='create_book_manually_rendered'),
     path('add/book/django-manual-form/', views.create_book_django_manual_form, name='create_book_django_manual_form'),
+    path('update/book/model-form/<int:book_pk>/', views.update_book_model_form, name='update_book_model_form'),
+    path('update/book/manual-form/<int:pk>/', views.update_book_manual_form, name='update_book_manual_form'),
+    path('confirm-delete/<int:pk>/', views.confirm_delete, name='confirm_delete'),
+    path('delete-book/<int:book_pk>/', views.delete_book, name='delete_book'),
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -77,3 +77,55 @@ def create_book_django_manual_form(request):
 
     context = {'form': form}
     return render(request, "library/create-book-django-manual-form.html", context)
+
+def update_book_model_form(request, book_pk):
+    book = get_object_or_404(Book, pk=book_pk)
+    form = BookForm(instance=book)
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES, instance=book)
+
+        if form.is_valid():
+            form.save()
+            return redirect('library:book_list')
+
+    context = {
+        'form': form,
+        'book': book,
+    }
+    return render(request, "library/update-book-model-form.html", context)
+
+
+def update_book_manual_form(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookManualForm(initial={
+        'title': book.title,
+        'author': book.author,
+        'number_of_pages': book.number_of_pages,
+        'published_on': book.published_on,
+        'cover_image': book.cover_image,
+    })
+    if request.method == "POST":
+        form = BookManualForm(request.POST, request.FILES)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            book.title = cleaned_data.get('title')
+            book.author = cleaned_data.get('author')
+            book.number_of_pages = cleaned_data.get('number_of_pages')
+            book.published_on = cleaned_data.get('published_on')
+            book.cover_image = cleaned_data.get('cover_image')
+            book.save()
+            return redirect('library:book_detail', book_id=pk)
+    context = {'form': form, 'book': book}
+    return render(request, "library/update-book-django-manual-form.html", context)   
+
+
+def confirm_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, "library/confirm-delete.html", {'book': book})
+
+
+def delete_book(request, book_pk):
+    book = get_object_or_404(Book, pk=book_pk)
+    if request.method == "POST":
+        book.delete()
+    return redirect("library:book_list")
